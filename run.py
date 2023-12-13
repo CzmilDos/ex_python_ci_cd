d5679e8f6a8f90dca67b81a9008cc19102e0cf19
Czmiley <czmild.montana@gmail.com>
2023-12-13 12:36:24 +0100

    correction linter...

--- run.py
+++ run.py
@@ -3,13 +3,24 @@ CI/CD
 """
 import pandas as pd
 
+
 def main():
     """
     Main
     """
-    import_data()
+    data = import_data()
+    data = rename_columns(data)
+    data_sample = sample_data(data)
+    multiply_dataset(data_sample)
+    data = create_total_sepal_column(data)
+    data = create_total_petal_column(data)
 
-    
+    print("Shape du DataFrame : ", data.shape)
+
+    print("Les deux premières lignes du DataFrame : ")
+    print(data.head(2))
+
+
 def import_data():
     """
     Import csv file as a dataframe
@@ -18,6 +29,79 @@ def import_data():
     data = pd.read_csv("data/iris.csv")
     print(data.shape)
     return data
+
+
+def rename_columns(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Rename dataframe columns
+    """
+    data_renamed = data.rename(columns={"sepal.length": 'sepal_length',
+        "sepal.width": 'sepal_width',
+        "petal.length": 'petal_length',
+        "petal.width": 'petal_width'
+        })
+    return data_renamed
+
+
+def sample_data(data: pd.DataFrame, sample_size: int = 50) -> pd.DataFrame:
+    """
+    Create a sample of the DataFrame with a specified number of rows.
+
+    Parameters:
+    - data (pd.DataFrame): The input DataFrame.
+    - sample_size (int): The number of rows to include in the sample. Default is 50.
+
+    Returns:
+    - sampled_data (pd.DataFrame): The DataFrame containing the sampled data.
+    """
+    sampled_data = data.sample(n=sample_size, random_state=42)  # random_state fixe pour assurer la reproductibilité des résult
+    return sampled_data
+
+
+def multiply_dataset(data: pd.DataFrame, multiplier: int = 3) -> pd.DataFrame:
+    """
+    Multiply the dataset by a specified factor using pd.concat.
+
+    Parameters:
+    - data (pd.DataFrame): The input DataFrame.
+    - multiplier (int): The factor by which to multiply the dataset. Default is 3.
+
+    Returns:
+    - multiplied_data (pd.DataFrame): The DataFrame containing the multiplied data.
+    """
+    # Utilise pd.concat pour concaténer le DataFrame original plusieurs fois
+    multiplied_data = pd.concat([data] * multiplier, ignore_index=True)
+    return multiplied_data
+
+
+def create_total_sepal_column(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Create a new column "total_sepal" in the DataFrame as the sum of "sepal_length" and "sepal_width".
+
+    Parameters:
+    - data (pd.DataFrame): The input DataFrame.
+
+    Returns:
+    - data_with_total_sepal (pd.DataFrame): The DataFrame with the new "total_sepal" column.
+    """
+    # Ajoute une nouvelle colonne "total_sepal" en faisant la somme de "sepal_length" et "sepal_width"
+    data_with_total_sepal = data.assign(total_sepal=data['sepal_length'] + data['sepal_width'])
+    return data_with_total_sepal
+
+
+def create_total_petal_column(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Create a new column "total_petal" in the DataFrame as the sum of "petal_length" and "petal_width".
+
+    Parameters:
+    - data (pd.DataFrame): The input DataFrame.
+
+    Returns:
+    - data_with_total_petal (pd.DataFrame): The DataFrame with the new "total_petal" column.
+    """
+    # Ajoute une nouvelle colonne "total_petal" en faisant la somme de "petal_length" et "petal_width"
+    data_with_total_petal = data.assign(total_petal=data['petal_length'] + data['petal_width'])
+    return data_with_total_petal
 
 
 if __name__ == '__main__':
--- test_run.py
+++ test_run.py
@@ -2,6 +2,13 @@
 Tests
 """
 from run import import_data
+from run import rename_columns
+from run import sample_data
+from run import multiply_dataset
+from run import create_total_sepal_column
+from run import create_total_petal_column
+import numpy as np
+import pandas as pd
 
 def test_import_data():
     """
@@ -9,3 +16,85 @@ def test_import_data():
     """
     data = import_data()
     assert data.shape[0] > 0
+
+
+def test_rename_columns():
+    """
+    Doc
+    """
+    data = import_data()
+    data_renamed = rename_columns(data)
+    assert "sepal_length" in data_renamed.columns
+
+def test_sample_data():
+    # Crée un DataFrame de test avec 100 lignes
+    test_data = pd.DataFrame({
+        'A': np.arange(1, 101),
+        'B': np.random.rand(100)
+    })
+
+    # Appelle la fonction sample_data pour obtenir un échantillon
+    sampled_data = sample_data(test_data, sample_size=50)
+
+    # Vérifie que le résultat est bien un DataFrame
+    assert isinstance(sampled_data, pd.DataFrame), "Le résultat n'est pas un DataFrame."
+
+    # Vérifie que le nombre de lignes dans l'échantillon est égal à sample_size
+    assert len(sampled_data) == 50, f"La taille de l'échantillon devrait être de 50, mais elle est de {len(sampled_data)}."
+
+def test_multiply_dataset():
+    # Crée un DataFrame de test avec 50 lignes
+    test_data = pd.DataFrame({
+        'A': np.arange(1, 51),
+        'B': np.random.rand(50)
+    })
+
+    # Appelle la fonction multiply_dataset pour obtenir un DataFrame multiplié
+    multiplied_data = multiply_dataset(test_data, multiplier=3)
+
+    # Vérifie que le résultat est bien un DataFrame
+    assert isinstance(multiplied_data, pd.DataFrame), "Le résultat n'est pas un DataFrame."
+
+    # Vérifie que le nombre de lignes dans le DataFrame multiplié est correct
+    expected_rows = len(test_data) * 3
+    assert len(multiplied_data) == expected_rows, f"La taille du DataFrame multiplié devrait être de {expected_rows}, mais elle est de {len(multiplied_data)}."
+
+def test_create_total_sepal_column():
+    # Crée un DataFrame de test avec 50 lignes
+    test_data = pd.DataFrame({
+        'sepal_length': np.random.rand(50),
+        'sepal_width': np.random.rand(50)
+    })
+
+    # Appelle la fonction create_total_sepal_column pour obtenir un DataFrame avec la nouvelle colonne
+    data_with_total_sepal = create_total_sepal_column(test_data)
+
+    # Vérifie que le résultat est bien un DataFrame
+    assert isinstance(data_with_total_sepal, pd.DataFrame), "Le résultat n'est pas un DataFrame."
+
+    # Vérifie que la colonne "total_sepal" a été ajoutée
+    assert 'total_sepal' in data_with_total_sepal.columns, "La colonne 'total_sepal' n'a pas été ajoutée."
+
+    # Vérifie que les valeurs dans la colonne "total_sepal" sont correctes
+    expected_values = test_data['sepal_length'] + test_data['sepal_width']
+    assert data_with_total_sepal['total_sepal'].equals(expected_values), "Les valeurs dans la colonne 'total_sepal' ne sont pas correctes."
+
+def test_create_total_petal_column():
+    # Crée un DataFrame de test avec 50 lignes
+    test_data = pd.DataFrame({
+        'petal_length': np.random.rand(50),
+        'petal_width': np.random.rand(50)
+    })
+
+    # Appelle la fonction create_total_petal_column pour obtenir un DataFrame avec la nouvelle colonne
+    data_with_total_petal = create_total_petal_column(test_data)
+
+    # Vérifie que le résultat est bien un DataFrame
+    assert isinstance(data_with_total_petal, pd.DataFrame), "Le résultat n'est pas un DataFrame."
+
+    # Vérifie que la colonne "total_petal" a été ajoutée
+    assert 'total_petal' in data_with_total_petal.columns, "La colonne 'total_petal' n'a pas été ajoutée."
+
+    # Vérifie que les valeurs dans la colonne "total_petal" sont correctes
+    expected_values = test_data['petal_length'] + test_data['petal_width']
+    assert data_with_total_petal['total_petal'].equals(expected_values), "Les valeurs dans la colonne 'total_petal' ne sont pas correctes."
